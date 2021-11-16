5a2ecc2cada563fee7775b43e797b4dccb759776
CLANNERETONNE <72856302+CLANNERETONNE@users.noreply.github.com>
2021-11-16 09:48:23 +0100

    2nd add

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+    public class HelloWorld_Exercice
+    {
+        public static string HelloWorld()
+        {
+            string string1 = "Hello World !";
+            return string1;
+        }
+    }
+}
